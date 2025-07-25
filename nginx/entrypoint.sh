92f8dcf1e7df213af64f968a8a44aece28dd8106
kntWT <kento.wata0628@gmail.com>
2025-07-25 17:30:18 +0900

    fix(web): credentialsに対応

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 envsubst < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
-sed -e "s/_host/\$host/g" -e "s/_remote_addr/\$remote_addr/g" -e "s/_proxy_add_x_forwarded_for/\$proxy_add_x_forwarded_for/g" -e "s/_schema/\$schema/g" /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.tmp
+sed -e "s/_host/\$host/g" -e "s/_remote_addr/\$remote_addr/g" -e "s/_proxy_add_x_forwarded_for/\$proxy_add_x_forwarded_for/g" -e "s/_schema/\$schema/g" -e "s/_http_origin/\$http_origin/g" /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.tmp
 mv /etc/nginx/conf.d/default.conf.tmp /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
